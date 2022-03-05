b4dbdc69488a1a950701dbd0737f07bbf6e0a014
kanishka kolanpyaka <kanishka.k0321@gmail.com>
2022-03-05 17:20:24 -0500

    docker changes

--- src/main/java/com/example/restservice/Greeting.java
+++ src/main/java/com/example/restservice/Greeting.java
@@ -4,10 +4,13 @@ public class Greeting {
 
 	private final long id;
 	private final String content;
+	private final String message;
 
-	public Greeting(long id, String content) {
+
+	public Greeting(long id, String content, String message) {
 		this.id = id;
 		this.content = content;
+		this.message = message;
 	}
 
 	public long getId() {
